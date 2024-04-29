87d5c7ff74a52a8be526643771b0e209c0839ccd
carlos <carlosvenegass8c@gmail.com>
2024-04-29 19:04:16 -0400

    feat: API usage

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,12 +1,20 @@
 import { SearchIcon } from "../components/Icon";
-interface Props { }
+interface Props {
+    setUser: (user: string) => void;
+ }
 
 function Form(props: Props) {
+    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+    const handleChangeUser = (e: any) => {
+        const value = e.target.value
+        props.setUser(value)
+        console.log(value)
+    }
     return (
         <form className="flex items-center bg-Secondary p-2 mt-9 mb-4 border rounded-2xl w-full">
             <SearchIcon />
-            <input type="text" className="w-full py-[6px]" placeholder="Search GitHub username..." />
-            <button className="bg-Primary border rounded-[10px] px-4 py-3 text-white">Search</button>
+            <input type="text" className="w-full py-[6px]" placeholder="Search GitHub username..." onChange={handleChangeUser}/>
+            <button type="button" className="bg-Primary border rounded-[10px] px-4 py-3 text-white">Search</button>
         </form>
     )
 }
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -13,7 +13,7 @@ function Heading(props: Props) {
         <div className="flex justify-between items-center mx-5">
           <h1 className={`${props.bgColor === 'Light' ? "text-textColorLight" : "text-textColorDark"} text-3xl`}>devfinder</h1>
           
-          <button className="p-[2.5px]" onClick={ToggleColor}>{props.bgColor === 'Light' ? (<span className="flex text-textColorSecondary hover:text-black">Dark <MoonIcon/></span>): (<span className="flex text-textColorDark hover:text-textColorSecondary">Light <SunIcon/></span>)}</button>
+          <button type="button" className="p-[2.5px]" onClick={ToggleColor}>{props.bgColor === 'Light' ? (<span className="flex text-textColorSecondary hover:text-black">Dark <MoonIcon/></span>): (<span className="flex text-textColorDark hover:text-textColorSecondary">Light <SunIcon/></span>)}</button>
         </div>
     )
 }
